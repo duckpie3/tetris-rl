071b11fae20ea314bc0eb2218e817495a3b12361
David <jdu@ciencias.unam.mx>
2025-10-16 17:50:35 -0600

    Remove env wrapper

--- checkenv.py
+++ checkenv.py
@@ -1,6 +1,6 @@
 from stable_baselines3.common.env_checker import check_env
-from tetris_env import TetrisEnv, ShiftWrapper
+from tetris_env import TetrisEnv
 
-env = ShiftWrapper(TetrisEnv())
+env = TetrisEnv()
 # It will check your custom environment and output additional warnings if needed
 check_env(env)
