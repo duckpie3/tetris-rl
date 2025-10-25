c0d80ec4a9aecd480865023dfc8aff14766487c8
David <jdu@ciencias.unam.mx>
2025-10-24 22:38:58 -0600

    Format

--- tetris_env.py
+++ tetris_env.py
@@ -19,6 +19,7 @@ WHITE = (255, 255, 255)
 
 FPS = 48
 
+
 class TetrisEnv(gym.Env):
     metadata = {"render_modes": ["human"], "render_fps": FPS}
 
@@ -29,12 +30,14 @@ class TetrisEnv(gym.Env):
         self.observation_space = spaces.Dict(
             spaces={
                 "piece_type": spaces.Box(low=1, high=7, shape=(1,), dtype=np.float32),
-                "rotation":   spaces.Box(low=0, high=3, shape=(1,), dtype=np.float32),
-                "x":          spaces.Box(low=-1, high=COLS-1, shape=(1,), dtype=np.float32),
-                "y":          spaces.Box(low=0, high=ROWS-1, shape=(1,), dtype=np.float32),
+                "rotation": spaces.Box(low=0, high=3, shape=(1,), dtype=np.float32),
+                "x": spaces.Box(low=-1, high=COLS - 1, shape=(1,), dtype=np.float32),
+                "y": spaces.Box(low=0, high=ROWS - 1, shape=(1,), dtype=np.float32),
                 "next_piece": spaces.Box(low=1, high=7, shape=(1,), dtype=np.float32),
-                "level":      spaces.Box(low=1, high=1000, shape=(1,), dtype=np.float32),
-                "board":      spaces.Box(low=0, high=1, shape=(ROWS * COLS,), dtype=np.float32),
+                "level": spaces.Box(low=1, high=1000, shape=(1,), dtype=np.float32),
+                "board": spaces.Box(
+                    low=0, high=1, shape=(ROWS * COLS,), dtype=np.float32
+                ),
             }
         )
         if self.render_mode == "human":
@@ -52,18 +55,22 @@ class TetrisEnv(gym.Env):
     def _get_observation(self):
         if self.tetris.next is None:
             raise ValueError
-        
+
         type_to_num = {"I": 1, "Z": 2, "S": 3, "J": 4, "L": 5, "T": 6, "O": 7}
 
         board = np.array(self.tetris.board)
         obs = {
-            "piece_type": np.array([type_to_num[self.tetris.figure.type]], dtype=np.float32),
-            "rotation":   np.array([self.tetris.figure.rotation], dtype=np.float32),
-            "x":          np.array([self.tetris.figure.x], dtype=np.float32),
-            "y":          np.array([self.tetris.figure.y], dtype=np.float32),
-            "next_piece": np.array([type_to_num[self.tetris.next.type]], dtype=np.float32),
-            "level":      np.array([self.tetris.level], dtype=np.float32),
-            "board":      (board != 0).astype(np.float32).flatten(),
+            "piece_type": np.array(
+                [type_to_num[self.tetris.figure.type]], dtype=np.float32
+            ),
+            "rotation": np.array([self.tetris.figure.rotation], dtype=np.float32),
+            "x": np.array([self.tetris.figure.x], dtype=np.float32),
+            "y": np.array([self.tetris.figure.y], dtype=np.float32),
+            "next_piece": np.array(
+                [type_to_num[self.tetris.next.type]], dtype=np.float32
+            ),
+            "level": np.array([self.tetris.level], dtype=np.float32),
+            "board": (board != 0).astype(np.float32).flatten(),
         }
         return obs
 
@@ -90,7 +97,7 @@ class TetrisEnv(gym.Env):
         return obs, info
 
     def step(self, action):
-        
+
         bumpiness_p = self.bumpiness
         hole_count_p = self.hole_count
         score_p = self.score
@@ -116,7 +123,7 @@ class TetrisEnv(gym.Env):
             reward += 0.01
         elif action == NONE:
             pass
-        
+
         if not freezed and self.frame >= self.next_gravity_frame:
             freezed = self.tetris.go_down()
             self.next_gravity_frame += self.fall_interval
@@ -127,7 +134,7 @@ class TetrisEnv(gym.Env):
             self.height = self.tetris.max_height
             self.hole_count = self.tetris.get_hole_count()
             self.score = self.tetris.score
-            
+
             line_bonus = [0.0, 1.0, 3.0, 5.0, 8.0][self.score - score_p]
             if line_bonus != 0.0:
                 self.steps_without_scoring = 0
@@ -139,23 +146,23 @@ class TetrisEnv(gym.Env):
             reward += -0.2 * (self.height - height_p)
 
             reward = float(np.clip(reward, -20.0, 20.0))
-        
+
         terminated = self.tetris.gameover
         truncated = self.steps_without_scoring >= self.steps_until_truncated
-            
+
         if self.tetris.gameover:
             reward -= 20.0
-        
+
         self.frame += 1
 
         if self.level != level_p and self.level <= 5:
-            self.fall_interval = self.base_fall_interval - 4*(self.level-1)
+            self.fall_interval = self.base_fall_interval - 4 * (self.level - 1)
 
         obs = self._get_observation()
 
         info = {}
         return obs, reward, terminated, truncated, info
-    
+
     def render(self):
         tetris = self.tetris
         if self.render_mode == "human":
@@ -167,7 +174,10 @@ class TetrisEnv(gym.Env):
                         img = self.Assets[val]
                         self.win.blit(img, (y * CELLSIZE, x * CELLSIZE))
                         pygame.draw.rect(
-                            self.win, WHITE, (y * CELLSIZE, x * CELLSIZE, CELLSIZE, CELLSIZE), 1
+                            self.win,
+                            WHITE,
+                            (y * CELLSIZE, x * CELLSIZE, CELLSIZE, CELLSIZE),
+                            1,
                         )
 
             if tetris.figure:
@@ -178,7 +188,9 @@ class TetrisEnv(gym.Env):
                             x = CELLSIZE * (tetris.figure.x + j)
                             y = CELLSIZE * (tetris.figure.y + i)
                             self.win.blit(img, (x, y))
-                            pygame.draw.rect(self.win, WHITE, (x, y, CELLSIZE, CELLSIZE), 1)
+                            pygame.draw.rect(
+                                self.win, WHITE, (x, y, CELLSIZE, CELLSIZE), 1
+                            )
 
             if tetris.gameover:
                 rect = pygame.Rect((50, 140, WIDTH - 100, HEIGHT - 350))
@@ -209,8 +221,7 @@ class TetrisEnv(gym.Env):
             pygame.draw.rect(self.win, BLUE, (0, 0, WIDTH, HEIGHT - 120), 2)
             self.clock.tick(FPS)
             pygame.display.update()
-        
-        
+
     def close(self):
         if self.render_mode == "human":
             pygame.quit
